1a831fbfdbfda308bef701b5889f9f834dda357d
AndreVidas <andre.v.olsen@gmail.com>
2017-09-04 11:15:12 +0200

    path changes

--- tests/run_tests.R
+++ tests/run_tests.R
@@ -1,10 +1,11 @@
 rm(list = ls())
+setwd("/data/user/andre/lipidomics/lipidQuan/R")
 library('RUnit')
 source('functions.R')
 
 
 test.suite <- defineTestSuite("auxFunctions",
-                              dirs = file.path("tests/testFiles"),
+                              dirs = file.path("../tests/testFiles"),
                               testFileRegexp = '*.R')
 
 
