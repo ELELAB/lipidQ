b155669a85f72c3b13776c563d564c4393549702
AndreVidas <andre.v.olsen@gmail.com>
2017-09-11 12:14:41 +0200

    REMOVED: #shinyApp(ui = ui, server = server)

--- R/runLipidQuan.R
+++ R/runLipidQuan.R
@@ -6,7 +6,6 @@
 #' @export
 #' @import shiny
 runLipidQuan <- function(){
-  #shinyApp(ui = ui, server = server)
   #runApp("inst/app/") # for test purposes
   suppressMessages(shiny::runApp(system.file("app", package = "lipidQuan"),launch.browser=TRUE)) # for builds
 }
