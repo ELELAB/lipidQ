3e0ef03bc0980b3b0b7d7c1eaaefd63317b7dceb
AndreVidas <andre.v.olsen@gmail.com>
2018-01-04 10:58:58 +0100

    change 'tableauOutput' variable name to 'output'

--- R/makeFinalOutput.R
+++ R/makeFinalOutput.R
@@ -1,8 +1,8 @@
-#' @title Make Tableau Output
+#' @title Make Final Output
 #' @author André Vidas Olsen
-#' @description This function creates an output file of the results in a format that can be used by Tableau.
-#' @param classPmol_molPctClass BLA BLA
-#' @param pmolCalculatedDataSet BLA BLA
+#' @description This function creates the final output file of the results which consists of both all filtered pmol% species and filtered pmol% classes.
+#' @param classPmol_molPctClass compact output file created by using the compactOutput_pmolCalc() script.
+#' @param pmolCalculatedDataSet data of pmol calculations made by using the pmolCalc() script.
 #' @export
 makeFinalOutput <- function(classPmol_molPctClass, pmolCalculatedDataSet){
 
@@ -29,13 +29,13 @@ makeFinalOutput <- function(classPmol_molPctClass, pmolCalculatedDataSet){
 
 
   # merge data sets together
-  tableauOutput <- rbind(lipidSpecies, classes)
+  output <- rbind(lipidSpecies, classes)
 
 
   # change classes/species with "O-": insert [SPACE] before "O-" and remove [SPACE] after "O-"
-  namesWithOIndexes <- grep("O-",tableauOutput$"mol%")
+  namesWithOIndexes <- grep("O-",output$"mol%")
   for(i in namesWithOIndexes){
-    nameWithO <- tableauOutput$"mol%"[i]
+    nameWithO <- output$"mol%"[i]
     nameWithO <- strsplit(nameWithO," ")
 
     if(length(nameWithO[[1]]) == 2){ # used when name represents a specie (contains number specs.)
@@ -43,8 +43,13 @@ makeFinalOutput <- function(classPmol_molPctClass, pmolCalculatedDataSet){
     }else{ # used when name represents a class (without number specs.)
       nameWithO <- paste0(gsub("O-", " O-", nameWithO[[1]][1]))
     }
-    tableauOutput$"mol%"[i] <- nameWithO
+    output$"mol%"[i] <- nameWithO
   }
 
-  return(tableauOutput)
+
+  # remove every row that only contains 0's in the columns
+  output <- output[apply(output[,-1], 1, FUN = function(x) !all(x == 0)),]
+
+
+  return(output)
 }
