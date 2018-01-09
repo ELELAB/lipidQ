8e919c453d06cfd374966c0ec5583c7ef8fedd24
AndreVidas <andre.v.olsen@gmail.com>
2018-01-09 16:39:31 +0100

    first commit

---
+++ R/plotQC_totalLipids.R
@@ -0,0 +1,123 @@
+#' @title picomol Calculation
+#' @author André Vidas Olsen
+#' @description This function creates QC plots of MS1 intensity data
+#' @param data data formatted by the use of the mergeDataSet function from LipidQuan.
+#' @param userSpecifiedColnames the column names template file containing user specified column names for the input data.
+#' @param numberOfReplicates the number of replicates for each sample
+#' @param blnkReplicates logical parameter for specifying whether the blank sample contains replicates or not. FALSE: no replicates, TRUE: replicates.
+#' @param pathToOutput the directory path to save the plots
+#' @import ggplot2
+#' @import reshape2
+#' @importFrom stats sd
+#' @export
+plotQC_totalLipids <- function(data, userSpecifiedColnames = NULL, pathToOutput, blnkReplicates = FALSE, numberOfReplicates){
+  # get colnames for data
+  dataColnames <- getColnames(userSpecifiedColnames = userSpecifiedColnames)
+
+  # find MS1x columns and BLNK column (last MS1x column)
+  #MS1x_names <- colnames(data)[grep(dataColnames$MS1x,colnames(data))] # names of all MS1x.* columns
+  #BLNK <- MS1x_names[length(MS1x_names)] # name of BLNK column (last MS1x.* column)
+  #MS1x_names <- MS1x_names[-length(MS1x_names)] # remove last column from MS1x_names since this is BLNK
+
+  # single blnk
+  if(blnkReplicates == FALSE){
+    #grep("^CLASS_PMOL_SUBT_PMOL_",colnames(data))
+    MS1x_names <- colnames(data)[grep("^CLASS_PMOL_SUBT_PMOL_",colnames(data))] # names of all CLASS_PMOL_SUBT_PMOL.* columns
+    #MS1x_names <- colnames(data)[grep(dataColnames$MS1x,colnames(data))] # names of all MS1x.* columns
+    BLNK <- MS1x_names[length(MS1x_names)] # name of BLNK column (last MS1x.* column)
+    #MS1x_names <- MS1x_names[-length(MS1x_names)] # remove last column from MS1x_names since this is BLNK
+
+  }
+  # blnk replicates
+  else{
+    MS1x_names <- colnames(data)[grep("^CLASS_PMOL_SUBT_PMOL_",colnames(data))] # names of all CLASS_PMOL_SUBT_PMOL.* columns
+    #MS1x_names <- colnames(data)[grep(dataColnames$MS1x,colnames(data))] # names of all MS1x.* columns
+    BLNK <- MS1x_names[(length(MS1x_names)-numberOfReplicates+1):length(MS1x_names)] # name of BLNK column (last MS1x.* column)
+    MS1x_names <- MS1x_names[-((length(MS1x_names)-numberOfReplicates+1):length(MS1x_names))] # remove last column from MS1x_names since this is BLNK
+  }
+
+
+  # find only species
+  isData <- data[-grep("^is",data[,dataColnames$SUM_COMPOSITION]),]
+
+
+
+  isData_colSums <- colSums(isData[grep("^CLASS_PMOL_SUBT_PMOL_",colnames(isData))], na.rm = TRUE)
+
+  data_pr_sample <- melt(isData_colSums)
+  #print(data_pr_sample)
+  #print(colnames(data_pr_sample))
+
+  #### plot total amount of lipids for each sample
+  p <- ggplot()
+  p <- p + geom_bar(data = data_pr_sample, aes_string(x = factor(1:nrow(data_pr_sample)), y = 'value'), stat = "identity", position = position_dodge(width = 0.9)) +
+    theme(plot.title = element_text(hjust = 0.5)) + labs(x = "Samples", y = "pmol") + scale_fill_discrete(guide = guide_legend(title = "Classes")) +
+    ggtitle("Total amount of lipids per sample")
+
+  ggsave(p, filename=paste0(pathToOutput, "/totalAmountOfLipids.png"), width = 14, height = 10, units = "cm")
+
+
+  #### plot all samples for a specific ISTD. Horizontal line = median of samples
+  #count <- 0
+
+  isData_SUM_C_and_CLASS <- cbind(isData[dataColnames$SUM_COMPOSITION], isData[grep("^CLASS_PMOL_SUBT_PMOL_",colnames(isData))])
+  #print(head(isData_SUM_C_and_CLASS))
+
+  count <- 0
+  for(name in isData_SUM_C_and_CLASS[,dataColnames$SUM_COMPOSITION]){
+    print(name)
+    data_pr_lipid <- melt(isData_SUM_C_and_CLASS[isData_SUM_C_and_CLASS[, dataColnames$SUM_COMPOSITION] == name, MS1x_names])
+    median <- median(data_pr_lipid$value)
+
+    #print(data_pr_lipid)
+
+
+    g <- ggplot()
+    g <- g + geom_bar(data = data_pr_lipid, aes_string(x = factor(1:nrow(data_pr_sample)), y = 'value'), stat="identity") +
+      theme(plot.title = element_text(hjust = 0.5)) + labs(x = "Sample", y = "pmol") + ggtitle(paste0("Total amount of lipid per sample\n(", name, ")")) +
+      geom_hline(aes(yintercept = median))
+
+    ggsave(g, filename=paste0(pathToOutput,"/", name,".png"), width = 14, height = 10, units = "cm")
+    count <- count + 1
+  }
+
+}
+
+
+
+
+
+# test
+setwd("/data/user/andre/lipidomics/lipidQuan")
+library(ggplot2)
+library(reshape2)
+source("R/mergeDataSets.R")
+source("R/sort_is.R")
+source("R/filterDataSet.R")
+source("R/pmolCalc.R")
+source("R/mergeFinalOutputs.R")
+source("R/readFile.R")
+source("R/rmSpaceInBeginning.R")
+source("R/getColnames.R")
+source("R/merge_endo_and_ISTD_db.R")
+source("R/filterReplicates.R")
+source("R/compactOutput_pmolCalc.R")
+source("R/makeFinalOutput.R")
+
+dataPathTest <- read.table("inst/extdata/test/LQ_Training/dataList.txt", stringsAsFactors = FALSE)[,1]
+endogene_lipid_db <- read.table("inst/extdata/test/LQ_Training/MS1_DB/LP_DB_MS1_v1.csv", stringsAsFactors = FALSE, header = TRUE, sep = ",")
+ISTD_lipid_db <- read.table("inst/extdata/test/LQ_Training/MS1_DB/ISTD_LP_DB_MS1_v1.csv", stringsAsFactors = FALSE, header = TRUE, sep = ",")
+list <- read.table("inst/extdata/test/LQ_Training/MS1_DB/userSpecifiedColnames.csv", stringsAsFactors = FALSE, header = TRUE, sep = ",")
+
+t <- mergeDataSets(dataList = dataPathTest, endogene_lipid_db = endogene_lipid_db, ISTD_lipid_db = ISTD_lipid_db, userSpecifiedColnames = list)
+t <- sort_is(t)
+t <- filterDataSet(data = t, endogene_lipid_db = endogene_lipid_db, ISTD_lipid_db = ISTD_lipid_db, userSpecifiedColnames = list)
+t <- pmolCalc(data = t,endogene_lipid_db = endogene_lipid_db, ISTD_lipid_db = ISTD_lipid_db, userSpecifiedColnames = list, spikeISTD = 2, zeroThresh = 0.25)
+classPmol_molPctClass <- compactOutput_pmolCalc(data = t, list)
+
+
+
+# run function
+plotQC_totalLipids(data = classPmol_molPctClass, userSpecifiedColnames = list, pathToOutput = "results/QC/post")
+
+
