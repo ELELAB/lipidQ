84f2661c2151b94c5ffd8fa440b1fd1865ea5a05
AndreVidas <andre.v.olsen@gmail.com>
2018-01-12 13:50:28 +0100

    implement QUAN_SCAN intols

--- R/plotQC_ISTD.R
+++ R/plotQC_ISTD.R
@@ -10,9 +10,17 @@
 #' @import reshape2
 #' @importFrom stats sd
 #' @export
-plotQC_ISTD <- function(data, userSpecifiedColnames = NULL, pathToOutput, blnkReplicates = FALSE, numberOfReplicates){
+plotQC_ISTD <- function(data, endogene_lipid_db, ISTD_lipid_db, userSpecifiedColnames = NULL, pathToOutput, blnkReplicates = FALSE, numberOfReplicates){
+
   # get colnames for data
   dataColnames <- getColnames(userSpecifiedColnames = userSpecifiedColnames)
+
+  # merge endogene_lipid_db and ISTD_lipid_db together
+  database <- merge_endo_and_ISTD_db(endogene_lipid_db, ISTD_lipid_db)
+
+  # create QUAN_SCAN column to data consisting of the QUAN_SCAN column in database.
+  data$QUAN_SCAN <- database$QUAN_SCAN[match(data[,dataColnames$SUM_COMPOSITION], database[,dataColnames$SUM_COMPOSITION])]
+
 
   # find MS1x columns and BLNK column (last MS1x column)
   #MS1x_names <- colnames(data)[grep(dataColnames$MS1x,colnames(data))] # names of all MS1x.* columns
@@ -37,11 +45,45 @@ plotQC_ISTD <- function(data, userSpecifiedColnames = NULL, pathToOutput, blnkRe
   # find only is classes
   isData <- data[grep("^is",data[,dataColnames$SUM_COMPOSITION]),]
 
-  # find median of MS1x values for each is class
-  isData[,paste0(dataColnames$MS1x,"_median")] <- apply(isData[, MS1x_names], 1, FUN = median)
+  for(i in 1:nrow(isData)){
 
-  isData[,paste0(dataColnames$MS1x,"_std")] <- apply(isData[, MS1x_names], 1, FUN = sd)
-  head(isData)
+    #MS1x <- gsub(unlist(strsplit(MS1x, "_"))[1], isData[i, "QUAN_SCAN"], MS1x)
+    #print(isData[i,])
+    #print(isData[i, "QUAN_SCAN"])
+
+    MS1x_names <- gsub(unlist(strsplit(MS1x_names, "_"))[1], isData[i, "QUAN_SCAN"], MS1x_names)
+    #print(MS1x_names)
+    print(isData[i, dataColnames$SUM_COMPOSITION])
+    print(MS1x_names)
+
+    # find median of MS1x values for each is class
+    #isData[,paste0(dataColnames$MS1x,"_median")] <- apply(isData[, MS1x_names], 1, FUN = median)
+    #print(isData[i, MS1x_names])
+    isData[i,paste0(dataColnames$MS1x,"_median")] <- median(as.numeric(isData[i, MS1x_names]))
+
+    #isData[,paste0(dataColnames$MS1x,"_std")] <- apply(isData[, MS1x_names], 1, FUN = sd)
+    isData[i,paste0(dataColnames$MS1x,"_std")] <- sd(as.numeric(isData[i, MS1x_names]))
+    #head(isData)
+
+
+
+
+    #### plot all samples for a specific ISTD. Horizontal line = median of samples
+    data_pr_ISTD <- melt(isData[isData[, dataColnames$SUM_COMPOSITION] == isData[i, dataColnames$SUM_COMPOSITION], MS1x_names])
+    median <- median(data_pr_ISTD$value)
+
+
+    g <- ggplot()
+    g <- g + geom_bar(data = data_pr_ISTD, aes_string(x = 'variable', y = 'value'), stat="identity") +
+      theme(axis.text.x = element_text(angle = 90, hjust = 1), plot.title = element_text(hjust = 0.5)) + labs(x = "Classes", y = "Intensity") + ggtitle(paste0("Sample intensity per ISTD\n(", isData[i, dataColnames$SUM_COMPOSITION], ")")) +
+      geom_hline(aes(yintercept = median))
+
+    ggsave(g, filename=paste0(pathToOutput,"/", isData[i, dataColnames$SUM_COMPOSITION],".png"), width = 14, height = 10, units = "cm")
+
+  }
+
+
+
 
   #### plot all ISTD's for averaged (median) sample intensities
   # ggplot2
@@ -58,21 +100,21 @@ plotQC_ISTD <- function(data, userSpecifiedColnames = NULL, pathToOutput, blnkRe
 
   #### plot all samples for a specific ISTD. Horizontal line = median of samples
   # ggplot2
-  count <- 0
-  for(name in isData[, dataColnames$SUM_COMPOSITION]){
-    data_pr_ISTD <- melt(isData[isData[, dataColnames$SUM_COMPOSITION] == name, MS1x_names])
-    median <- median(data_pr_ISTD$value)
-
-
-    g <- ggplot()
-    g <- g + geom_bar(data = data_pr_ISTD, aes_string(x = 'variable', y = 'value'), stat="identity") +
-      theme(axis.text.x = element_text(angle = 90, hjust = 1), plot.title = element_text(hjust = 0.5)) + labs(x = "Classes", y = "Intensity") + ggtitle(paste0("Sample intensity per ISTD\n(", name, ")")) +
-      geom_hline(aes(yintercept = median))
-
-    ggsave(g, filename=paste0(pathToOutput,"/", name,".png"), width = 14, height = 10, units = "cm")
-
-    count <- count + 1
-  }
+#  count <- 0
+#  for(name in isData[, dataColnames$SUM_COMPOSITION]){
+#    data_pr_ISTD <- melt(isData[isData[, dataColnames$SUM_COMPOSITION] == name, MS1x_names])
+#    median <- median(data_pr_ISTD$value)
+#
+#
+#    g <- ggplot()
+#    g <- g + geom_bar(data = data_pr_ISTD, aes_string(x = 'variable', y = 'value'), stat="identity") +
+#      theme(axis.text.x = element_text(angle = 90, hjust = 1), plot.title = element_text(hjust = 0.5)) + labs(x = "Classes", y = "Intensity") + ggtitle(paste0("Sample intensity per ISTD\n(", name, ")")) +
+#      geom_hline(aes(yintercept = median))
+#
+#    ggsave(g, filename=paste0(pathToOutput,"/", name,".png"), width = 14, height = 10, units = "cm")
+#
+#    count <- count + 1
+#  }
 
 }
 
@@ -80,13 +122,25 @@ plotQC_ISTD <- function(data, userSpecifiedColnames = NULL, pathToOutput, blnkRe
 
 
 
+source("R/mergeDataSets.R")
+source("R/sort_is.R")
+source("R/filterDataSet.R")
+source("R/pmolCalc.R")
+source("R/mergeFinalOutputs.R")
+source("R/readFile.R")
+source("R/rmSpaceInBeginning.R")
+source("R/getColnames.R")
+source("R/merge_endo_and_ISTD_db.R")
+source("R/filterReplicates.R")
+library(ggplot2)
+library(reshape2)
+dataPathTest <- read.table("inst/extdata/test/LQ_Training/dataList.txt", stringsAsFactors = FALSE)[,1]
+endogene_lipid_db <- read.table("inst/extdata/test/LQ_Training/MS1_DB/LP_DB_MS1_v1.csv", stringsAsFactors = FALSE, header = TRUE, sep = ",")
+ISTD_lipid_db <- read.table("inst/extdata/test/LQ_Training/MS1_DB/ISTD_LP_DB_MS1_v1.csv", stringsAsFactors = FALSE, header = TRUE, sep = ",")
+list <- read.table("inst/extdata/test/LQ_Training/MS1_DB/userSpecifiedColnames.csv", stringsAsFactors = FALSE, header = TRUE, sep = ",")
 
-
-data <- read.csv("results/dataTables/mergedDataSets.csv")
-
-plotQC_ISTD(data, userSpecifiedColnames = NULL, pathToOutput = "results/QC/pre", blnkReplicates = TRUE, numberOfReplicates = 3)
-
-
+t <- mergeDataSets(dataList = dataPathTest, endogene_lipid_db = endogene_lipid_db, ISTD_lipid_db = ISTD_lipid_db, userSpecifiedColnames = list)
+plotQC_ISTD(data = data, endogene_lipid_db = endogene_lipid_db, ISTD_lipid_db = ISTD_lipid_db, userSpecifiedColnames = NULL, pathToOutput = "results/QC/pre", blnkReplicates = TRUE, numberOfReplicates = 3)
 
 
 
