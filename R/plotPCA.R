b0dc7e04202ac5311db28c8cf7278a60c395fe95
AndreVidas <andre.v.olsen@gmail.com>
2018-01-30 16:17:04 +0100

    Implement classes part. Before was only the specie plots

--- R/plotPCA.R
+++ R/plotPCA.R
@@ -20,6 +20,7 @@ plotPCA <- function(data, groups, pathToOutput){
   mol_pct_classes_cols <- data[-grep(":",data$mol.),2:ncol(data)]
 
   species <- data[grep(":",data$mol.),1]
+  classes <- data[-grep(":",data$mol.),1]
 
 
 
@@ -35,24 +36,41 @@ plotPCA <- function(data, groups, pathToOutput){
     type[unlist(ranges[colnames(groups)[i]])] <- colnames(groups)[i]
   }
 
+  # insert row and col names to be used for the biplots
   rownames(mol_pct_species_cols) <- species
   colnames(mol_pct_species_cols) <- paste0(colnames(mol_pct_species_cols),"_",type)
 
+  rownames(mol_pct_classes_cols) <- classes
+  colnames(mol_pct_classes_cols) <- paste0(colnames(mol_pct_classes_cols),"_",type)
 
 
-  pca <- prcomp(mol_pct_species_cols, scale=TRUE, center= TRUE)
+
+  pca_species <- prcomp(mol_pct_species_cols, scale=TRUE, center= TRUE)
+  pca_classes <- prcomp(mol_pct_classes_cols, scale=TRUE, center= TRUE)
 
 
-  screePlot <- fviz_eig(pca)
-  ggsave(screePlot, filename=paste0(pathToOutput, "/screePlot.png"), width = 40, height = 25, units = "cm")
+  #### scree plot for species and classes
+  screePlot_species <- fviz_eig(pca_species)
+  ggsave(screePlot_species, filename=paste0(pathToOutput, "/screePlot_species.png"), width = 40, height = 25, units = "cm")
+
+  screePlot_classes <- fviz_eig(pca_classes)
+  ggsave(screePlot_classes, filename=paste0(pathToOutput, "/screePlot_classes.png"), width = 40, height = 25, units = "cm")
 
 
-  biplot <-fviz_pca_biplot(pca, repel = TRUE,
+
+  #### biplot for species and classes
+  biplot_species <-fviz_pca_biplot(pca_species, repel = TRUE,
                   col.var = "#2E9FDF", # Variables color
                   col.ind = "#696969"  # Individuals color
   )
+  ggsave(biplot_species, filename=paste0(pathToOutput, "/PCA_biplot_species.png"), width = 40, height = 25, units = "cm")
 
-  ggsave(biplot, filename=paste0(pathToOutput, "/PCA_biplot.png"), width = 40, height = 25, units = "cm")
+  biplot_classes <-fviz_pca_biplot(pca_classes, repel = TRUE,
+                           col.var = "#2E9FDF", # Variables color
+                           col.ind = "#696969"  # Individuals color
+  )
+  ggsave(biplot_classes, filename=paste0(pathToOutput, "/PCA_biplot_classes.png"), width = 40, height = 25, units = "cm")
+
 }
 
 
