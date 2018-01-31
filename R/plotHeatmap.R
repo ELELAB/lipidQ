a7e842afb8a6990d5f040b58ceea4c54c6864b52
AndreVidas <andre.v.olsen@gmail.com>
2018-01-31 14:32:02 +0100

    Implement finding optimal K clusters, by using the NbClust R package

--- R/plotHeatmap.R
+++ R/plotHeatmap.R
@@ -17,6 +17,7 @@
 #' @importFrom stats cor
 #' @importFrom ComplexHeatmap Heatmap HeatmapAnnotation
 #' @importFrom circlize colorRamp2
+#' @importFrom NbClust NbClust
 #' @export
 plotHeatmap <- function(data, groups, K = NULL, pathToOutput) {
 
@@ -48,6 +49,44 @@ plotHeatmap <- function(data, groups, K = NULL, pathToOutput) {
   names(colors) <- unique(type)
 
 
+  print(head(data))
+  #### specify the optimal number of K
+  if(is.null(K)){
+    nb <- NbClust(data[,2:ncol(data)], distance = "euclidean", min.nc = 2,
+                  max.nc = ncol(data[,2:ncol(data)]), method = "kmeans")
+
+
+    t <- as.data.frame(table(nb$Best.nc[1,]), stringsAsFactors = FALSE)
+    #print(t)
+    t <- t[1:(nrow(t)-1),] # remove highest cluster (K == number of samples) from the possible optimal cluster list, since ComplexHeatmap can't have same number of clusters as observations
+    #print(t)
+    listOfK <- as.numeric(t[t$Freq == max(t$Freq), "Var1"])
+    #print(listOfK)
+
+
+    # for test
+    #t <- as.data.frame(table(c(1,1,2,2,2,1,2,3,4,4,4,4)), stringsAsFactors = FALSE)
+    #t <- t[1:(nrow(t)-1),]
+    #print(t)
+    #listOfK <- as.numeric(t[t$Freq == max(t$Freq), "Var1"])
+    #print(listOfK)
+
+    # find next highest K, if K = number of samples in data (ComplexHeatmap can't have same number of clusters as observations)
+    #if(max(listOfK) == ncol(data[,2:ncol(data)])){
+    #  mostOptimalRemoved <- t[!(t$Var1 == max(t$Var1)),]
+    #  listOfK <- as.numeric(mostOptimalRemoved[mostOptimalRemoved$Freq == max(mostOptimalRemoved$Freq), "Var1"])
+    #}
+  }else{
+    listOfK <- K
+  }
+  print(listOfK)
+
+
+
+
+  # make graphs for each optimal number of K's (there will be multiples if there are equally high number of choices for severeal K's)
+  for(K in listOfK){
+
   #### heatmap species
   dataMatrix <- data.matrix(mol_pct_species_cols)
   rownames(dataMatrix) <- data[1:nrow(mol_pct_species_cols),1]
@@ -65,7 +104,7 @@ plotHeatmap <- function(data, groups, K = NULL, pathToOutput) {
   heat <- Heatmap(dataMatrix, name = "mol pct.", column_title = "Classes", column_title_side = "bottom", row_title = "Samples", row_title_side = "right", na_col = "black", col = colorRamp2(c(0, median(dataMatrix, na.rm = TRUE), max(dataMatrix, na.rm = TRUE)), c("white", "yellow", "red")), show_row_dend = FALSE, cluster_rows = TRUE, km = K)
   heatPlot <- ha + heat
 
-  png(file = paste0(pathToOutput, "/heatmapSpecies.png"), height = 800, width = (22*nrow(mol_pct_species_cols)))
+  png(file = paste0(pathToOutput, "/heatmapSpecies_K_", K, ".png"), height = 800, width = (22*nrow(mol_pct_species_cols)))
   draw(heatPlot)
   dev.off()
 
@@ -88,10 +127,11 @@ plotHeatmap <- function(data, groups, K = NULL, pathToOutput) {
   ha <- HeatmapAnnotation(df = data.frame(type = type), which = "row", col = list(type = colors))
   heat <- Heatmap(dataMatrix, name = "mol pct.", column_title = "Classes", column_title_side = "bottom", row_title = "Samples", row_title_side = "right", na_col = "black", col = colorRamp2(c(0, median(dataMatrix, na.rm = TRUE), max(dataMatrix, na.rm = TRUE)), c("white", "yellow", "red")), show_row_dend = FALSE, cluster_rows = TRUE, km = K)
   heatPlot <- ha + heat
-  png(file = paste0(pathToOutput, "/heatmapClasses.png"), height = 800, width = (22*nrow(mol_pct_classes_cols)))
+  png(file = paste0(pathToOutput, "/heatmapClasses_K_", K, ".png"), height = 800, width = (22*nrow(mol_pct_classes_cols)))
   draw(heatPlot)
   dev.off()
 
+  }
 }
 
 
@@ -101,12 +141,8 @@ plotHeatmap <- function(data, groups, K = NULL, pathToOutput) {
 #data <- read.csv("results/dataTables/finalOutput_molPct.csv")
 #colnames(data)
 
-plotHeatmap(data = data, groups = groups, K = 2, pathToOutput = "/data/user/andre/lipidomics/lipidQuan/")
+plotHeatmap(data = data, groups = groups, pathToOutput = "/data/user/andre/lipidomics/lipidQuan/")
 
 
-
-
-
-# test with
 
 
