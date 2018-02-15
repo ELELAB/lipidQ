4c6eaf9e5459e74288795fcf8e2b2f336df44c47
AndreVidas <andre.v.olsen@gmail.com>
2018-02-15 15:19:38 +0100

    decrease lines to below 80 characters

--- R/sort_is.R
+++ R/sort_is.R
@@ -1,14 +1,24 @@
 #' @title Sort Internal Standards
 #' @author André Vidas Olsen
-#' @description This function orders all internal standards (is) in a given data set so that they appear as the last rows
-#' (after the normal species). The purpose for this is mainly to get a better overview of the data types when manually inspecting the data.
-#' @param data data formatted by the use of the mergeDataSet function from LipidQuan.
-#' @param userSpecifiedColnames the column names template file containing user specified column names for the input data. This file
+#' @description This function orders all internal standards (is) in a given data
+#' set so that they appear as the last rows
+#' (after the normal species). The purpose for this is mainly to get a better
+#' overview of the data types when manually inspecting the data.
+#' @param data data formatted by the use of the mergeDataSet function from
+#' LipidQuan.
+#' @param userSpecifiedColnames the column names template file containing user
+#' specified column names for the input data. This file
 #' @export
 #' @return a sorted data set
 #' @examples
-#' mergedDataSets <- read.table(system.file("extdata/dataTables", "mergedDataSets.csv", package = "lipidQuan"), stringsAsFactors = FALSE, header = TRUE, sep = ",")
-#' list <- read.table(system.file("extdata/LipidQ_DataBase", "userSpecifiedColnames.csv", package = "lipidQuan"), stringsAsFactors = FALSE, header = TRUE, sep = ",")
+#' mergedDataSets <- read.table(system.file("extdata/dataTables",
+#'  "mergedDataSets.csv", package = "lipidQuan"), stringsAsFactors = FALSE,
+#'  header = TRUE, sep = ",")
+#'
+#' list <- read.table(system.file("extdata/LipidQ_DataBase",
+#'  "userSpecifiedColnames.csv", package = "lipidQuan"),
+#'  stringsAsFactors = FALSE, header = TRUE, sep = ",")
+#'
 #' sortedData <- sort_is(mergedDataSets, userSpecifiedColnames = list)
 sort_is<-function(data, userSpecifiedColnames = NULL){
 
