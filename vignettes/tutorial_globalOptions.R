0d9ecefc206b156ac3da1d2f6b92f6df91571734
AndreVidas <andre.v.olsen@gmail.com>
2018-02-15 15:09:58 +0100

    updates

--- vignettes/tutorial_globalOptions.R
+++ vignettes/tutorial_globalOptions.R
@@ -12,14 +12,16 @@ system.file("app/", package = "lipidQuan")
 
 ## ----echo = FALSE--------------------------------------------------------
 library(knitr)
-data <- read.csv(system.file("extdata/LipidQ_DataBase/userSpecifiedColnames.csv", package = "lipidQuan"), stringsAsFactors = FALSE)
+data <- read.csv(system.file("extdata/LipidQ_DataBase/userSpecifiedColnames.csv"
+                             , package = "lipidQuan"), stringsAsFactors = FALSE)
 data[1,] <- "TYPE_NAME_HERE"
 kable(data[,1:7])
 kable(data[,8:ncol(data)])
 
 ## ----echo = FALSE--------------------------------------------------------
 library(knitr)
-data <- read.csv(system.file("extdata/LipidQ_DataBase/userSpecifiedColnames.csv", package = "lipidQuan"), stringsAsFactors = FALSE)
+data <- read.csv(system.file("extdata/LipidQ_DataBase/userSpecifiedColnames.csv"
+                             , package = "lipidQuan"), stringsAsFactors = FALSE)
 kable(data[,1:7])
 kable(data[,8:ncol(data)])
 
