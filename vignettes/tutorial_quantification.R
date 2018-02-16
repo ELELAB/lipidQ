ca6166fc9a8d0db77611b2fcd461107dd22a5460
AndreVidas <andre.v.olsen@gmail.com>
2018-02-16 16:16:19 +0100

    add secondary files again

---
+++ vignettes/tutorial_quantification.R
@@ -0,0 +1,15 @@
+## ----setup, include = FALSE----------------------------------------------
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  comment = "#>"
+)
+
+## ---- echo=FALSE---------------------------------------------------------
+system.file("app/", package = "lipidQuan")
+
+## ---- echo=FALSE---------------------------------------------------------
+system.file("extdata/mE504_Data/", package = "lipidQuan")
+
+## ---- echo=FALSE---------------------------------------------------------
+system.file("extdata/LipidQ_DataBase", package = "lipidQuan")
+
